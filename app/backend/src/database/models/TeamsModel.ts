fdc819ca6b937750938f430bddcb6f256592d2b0
anadolacio <anadolacio@hotmail.com>
2023-06-13 18:10:42 -0300

    resolvendo lint

--- app/backend/src/database/models/TeamsModel.ts
+++ app/backend/src/database/models/TeamsModel.ts
@@ -8,29 +8,31 @@ import {
 
 import db from '.';
   
-class TeamsModel extends Model<InferAttributes<TeamsModel>,
-  InferCreationAttributes<TeamsModel>> {
-declare id: CreationOptional<number>;
-declare teamName: CreationOptional<number>;
+class TeamsModel extends Model<
+InferAttributes<TeamsModel>,
+InferCreationAttributes<TeamsModel>
+> {
+  declare id: CreationOptional<number>;
+  declare teamName: CreationOptional<number>;
 }
 
 TeamsModel.init({
   id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    teamName: {
-        type: DataTypes.STRING(30),
-        allowNull: false,
-      }
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  teamName: {
+    type: DataTypes.STRING(30),
+    allowNull: false,
+  }
   }, {
-    sequelize: db,
-    modelName: 'teams',
-    timestamps: false,
-    underscored: true,
+     sequelize: db,
+     modelName: 'teams',
+     timestamps: false,
+     underscored: true,
   });
  
-  export default TeamsModel;
+export default TeamsModel;
   
